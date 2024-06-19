25f9a26d58df1cc9171262d94791d83fba2fe238
EllinaObukhova <ellina.obukhova@redcollar.ru>
2024-06-19 12:34:40 +0300

    creating init class-structure

---
+++ src/db/DBImitator.java
@@ -0,0 +1,11 @@
+package db;
+
+public class DBImitator {
+    /**
+     * there go several maps aka tables
+     */
+
+    /**
+     * there go gettes (no setters, only special method init db)
+     */
+}
