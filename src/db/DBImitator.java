648a5970695047c81739be9e8085e29f27183b9d
EllinaObukhova <ellina.obukhova@redcollar.ru>
2024-06-19 12:57:30 +0300

    DBImitator: dicts, getters, initmethod

--- src/db/DBImitator.java
+++ src/db/DBImitator.java
@@ -1,11 +1,35 @@
 package db;
 
-public class DBImitator {
-    /**
-     * there go several maps aka tables
-     */
+import db.entity.OrderEntity;
+import db.entity.OrderLineEntity;
+import db.entity.ProductEntity;
 
-    /**
-     * there go gettes (no setters, only special method init db)
-     */
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class DBImitator {
+    private Map<UUID, OrderEntity> ordersTable;
+    private Map<UUID, OrderLineEntity> orderLineTable;
+    private Map<UUID, ProductEntity> productTable;
+
+    public void initDBState() {
+        ordersTable = new HashMap<>();
+        orderLineTable = new HashMap<>();
+        productTable = new HashMap<>();
+    }
+
+    //todo add mockEntityCreation methods
+
+    public Map<UUID, OrderEntity> getOrdersTable() {
+        return ordersTable;
+    }
+
+    public Map<UUID, OrderLineEntity> getOrderLineTable() {
+        return orderLineTable;
+    }
+
+    public Map<UUID, ProductEntity> getProductTable() {
+        return productTable;
+    }
 }
