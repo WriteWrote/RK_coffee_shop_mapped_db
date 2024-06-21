d23acc3cfa6a7a740110532ab845d020d88357e5
EllinaObukhova <ellina.obukhova@redcollar.ru>
2024-06-21 21:21:54 +0300

    init methods for db

--- src/db/DBImitator.java
+++ src/db/DBImitator.java
@@ -4,32 +4,70 @@ import db.entity.OrderEntity;
 import db.entity.OrderLineEntity;
 import db.entity.ProductEntity;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class DBImitator {
-    private Map<UUID, OrderEntity> ordersTable;
-    private Map<UUID, OrderLineEntity> orderLineTable;
-    private Map<UUID, ProductEntity> productTable;
-
-    public void initDBState() {
-        ordersTable = new HashMap<>();
-        orderLineTable = new HashMap<>();
-        productTable = new HashMap<>();
-    }
-
-    //todo add mockEntityCreation methods
-
-    public Map<UUID, OrderEntity> getOrdersTable() {
-        return ordersTable;
-    }
-
-    public Map<UUID, OrderLineEntity> getOrderLineTable() {
-        return orderLineTable;
-    }
-
-    public Map<UUID, ProductEntity> getProductTable() {
-        return productTable;
-    }
+	private Map<UUID, OrderEntity> ordersTable;
+	private Map<UUID, OrderLineEntity> orderLineTable;
+	private Map<UUID, ProductEntity> productTable;
+	
+	public void initEmptyDBState() {
+		ordersTable = new HashMap<>();
+		orderLineTable = new HashMap<>();
+		productTable = new HashMap<>();
+	}
+	
+	public void initDBWithMockData(int nProducts, int nOrders) {
+		ordersTable = new HashMap<>();
+		orderLineTable = new HashMap<>();
+		productTable = new HashMap<>();
+		
+		for (int i = 0; i < nProducts; i++) {
+			UUID generatedUuid = UUID.randomUUID();
+			productTable.put(
+					generatedUuid,
+					new ProductEntity(
+							generatedUuid,
+							"product" + i,
+							"description" + i
+					)
+			);
+		}
+		for (int i = 0; i < nOrders; i++) {
+			UUID generatedUuid = UUID.randomUUID();
+			ordersTable.put(
+					generatedUuid,
+					new OrderEntity(
+							generatedUuid,
+							null,
+							"88005553535",
+							"Ivanov Ivan Ivanovich",
+							"Russia",
+							"CREATED"
+					)
+			);
+			int randomProductNumber = ThreadLocalRandom.current().nextInt(0, productTable.size());
+			orderLineTable.put(
+					UUID.randomUUID(),
+					new OrderLineEntity(
+							Objects.requireNonNull(productTable.entrySet().stream().skip(randomProductNumber).findFirst().orElse(null)).getValue().getId(),
+							generatedUuid,
+							ThreadLocalRandom.current().nextInt(0, 10 + 1)
+					)
+			);
+		}
+	}
+	
+	public Map<UUID, OrderEntity> getOrdersTable() {
+		return ordersTable;
+	}
+	
+	public Map<UUID, OrderLineEntity> getOrderLineTable() {
+		return orderLineTable;
+	}
+	
+	public Map<UUID, ProductEntity> getProductTable() {
+		return productTable;
+	}
 }
