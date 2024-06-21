037364721548ea44a5d15afcb71e6f143ef6151c
EllinaObukhova <ellina.obukhova@redcollar.ru>
2024-06-21 21:46:07 +0300

    template for abstract crud repository

--- src/db/repository/CommonCRUDRepository.java
+++ src/db/repository/CommonCRUDRepository.java
@@ -1,4 +1,35 @@
 package db.repository;
 
-public class CommonCRUDRepository {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public abstract class CommonCRUDRepository<T> {
+	/**
+	 * CRUD repository
+	 * create
+	 * delete
+	 * update
+	 * findById
+	 * findAll
+	 */
+	public T create() {
+		return (T) new Object();    //there goes reflection?
+	}
+	
+	public void delete() {
+	
+	}
+	
+	public T update() {
+		return (T) new Object();
+	}
+	
+	public T findById(UUID id) {
+		return (T) new Object();
+	}
+	
+	public List<T> findAll() {
+		return new ArrayList<T>();
+	}
 }
