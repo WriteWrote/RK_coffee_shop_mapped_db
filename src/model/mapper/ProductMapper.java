2b2abacb88d598147c461e7787c4e3c79d7dc1cf
EllinaObukhova <ellina.obukhova@redcollar.ru>
2024-06-22 19:52:43 +0300

    services: ProductService.java

---
+++ src/model/mapper/ProductMapper.java
@@ -0,0 +1,13 @@
+package model.mapper;
+
+import db.entity.ProductEntity;
+import model.dto.ProductDto;
+
+public class ProductMapper {
+	public ProductEntity toEntity(ProductDto dto){
+		return null;	//todo not yet implemented
+	}
+	public ProductDto fromEntity(ProductEntity entity){
+		return null;	//todo not yet implemented
+	}
+}
--- src/service/ProductService.java
+++ src/service/ProductService.java
@@ -1,4 +1,27 @@
 package service;
 
+import db.repository.ProductRepository;
+import model.dto.ProductDto;
+import model.mapper.ProductMapper;
+
 public class ProductService {
+	private final ProductRepository repository;
+	private final ProductMapper mapper;
+	
+	public ProductService(ProductRepository repository, ProductMapper mapper) {
+		this.repository = repository;
+		this.mapper = mapper;
+	}
+	
+	public void createProduct(ProductDto dto) {  //todo decide whether use Responce or Dto
+		repository.save(mapper.toEntity(dto));
+	}
+	
+	public void deleteProduct() {
+		//todo not yet implemented
+	}
+	
+	public void updateProduct() {
+		//todo not yet implemented
+	}
 }
