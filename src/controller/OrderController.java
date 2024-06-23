50d8133383ea16672b9b12ad54dab3b1001b69d3
EllinaObukhova <ellina.obukhova@redcollar.ru>
2024-06-23 13:31:49 +0300

    controllers: OrderController.java ExceptionHandler.java

--- src/controller/OrderController.java
+++ src/controller/OrderController.java
@@ -1,4 +1,32 @@
 package controller;
 
+import model.dto.OrderDto;
+import service.OrderService;
+
+import java.util.List;
+import java.util.UUID;
+
 public class OrderController {
+	private final OrderService service;
+	private final ExceptionHandler<OrderDto> handler;
+	
+	public OrderController(OrderService service, ExceptionHandler<OrderDto> handler) {
+		this.service = service;
+		this.handler = handler;
+	}
+	
+	public void create(OrderDto dto) { System.out.println(handler.handle(() -> service.create(dto))); }
+	
+	public void delete(UUID uuid) {
+		System.out.println(handler.handle(() -> {
+			service.delete(uuid);
+			return null;
+		}));
+	}
+	
+	public void update(OrderDto dto) { System.out.println(handler.handle(() -> service.update(dto))); }
+	
+	public void getById(UUID uuid) { System.out.println(handler.handle(() -> service.getById(uuid))); }
+	
+	public void getAll() { System.out.println(new ExceptionHandler<List<OrderDto>>().handle(service::getAll)); }
 }
